7b5eeac7f4216ae2a3bd5857213007bd18f31094
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-05-09 13:45:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ const modalOverlay = document.getElementById('modalOverlay');
+
+    function openModal() {
+      modalOverlay.style.display = 'flex';
+    }
+
+    function closeModal() {
+      modalOverlay.style.display = 'none';
+    }
+
+    function handleOverlayClick(event) {
+      if (event.target === modalOverlay) {
+        closeModal();
+      }
+    }
